ed25146b239498bd20b2e5dbdc7a4db447bc26dd
Louis-C7 <972339262@qq.com>
2023-11-13 18:37:39 +0800

    add harmony support

--- packages/default-storage-backend/src/RCTAsyncStorage.ts
+++ packages/default-storage-backend/src/RCTAsyncStorage.ts
@@ -1,14 +1,16 @@
 // @ts-ignore Module '"react-native"' has no exported member 'TurboModuleRegistry'.
-import { NativeModules, TurboModuleRegistry } from "react-native";
+import { NativeModules, TurboModuleRegistry, Platform } from "react-native";
 import { shouldFallbackToLegacyNativeModule } from "./shouldFallbackToLegacyNativeModule";
 
 // TurboModuleRegistry falls back to NativeModules so we don't have to try go
 // assign NativeModules' counterparts if TurboModuleRegistry would resolve
 // with undefined.
 let RCTAsyncStorage = TurboModuleRegistry
-  ? TurboModuleRegistry.get("PlatformLocalStorage") || // Support for external modules, like react-native-windows
-    TurboModuleRegistry.get("RNC_AsyncSQLiteDBStorage") ||
-    TurboModuleRegistry.get("RNCAsyncStorage")
+  ? Platform.OS === "harmony"
+    ? TurboModuleRegistry.get("RNCAsyncStorage")
+    : TurboModuleRegistry.get("PlatformLocalStorage") || // Support for external modules, like react-native-windows
+      TurboModuleRegistry.get("RNC_AsyncSQLiteDBStorage") ||
+      TurboModuleRegistry.get("RNCAsyncStorage")
   : NativeModules["PlatformLocalStorage"] || // Support for external modules, like react-native-windows
     NativeModules["RNC_AsyncSQLiteDBStorage"] ||
     NativeModules["RNCAsyncStorage"];
